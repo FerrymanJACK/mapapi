480804e757e353ed79f41b12f0a4d07267f47262
wangrongqing <wangrongqingJACK@163.com>
2018-11-27 14:59:49 +0800

    1.3.1 地图中英文切换； 1.3.2 获取中心点坐标与zoom； 1.3.3 根据搜索跳转城市失败...

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
-
 export default new Router({
-  routes: [                 //路由数组，当前只包含一个路由,path, componnets
+  routes: [
     {
       path: '/',
       name: 'HelloWorld',
