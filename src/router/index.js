7ce2db29585708adc4a315e6744933da1fe698d7
wangrongqing <wangrongqingJACK@163.com>
2018-11-27 11:55:51 +0800

    更新vue router 1.0

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import HelloWorld from '@/components/HelloWorld'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: [                 //路由数组，当前只包含一个路由,path, componnets
     {
       path: '/',
       name: 'HelloWorld',
